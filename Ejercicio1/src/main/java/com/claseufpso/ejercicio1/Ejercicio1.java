96d7f13de9f2807afac531ce45e560d4a0813851
Jean Pierre <jpgarciad@ufpso.edu.co>
2024-02-29 15:24:52 -0500

    Creación de clase interna

--- Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/main/java/com/claseufpso/ejercicio1/Ejercicio1.java
@@ -3,6 +3,29 @@ package com.claseufpso.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character Homero = new Character("Homero", 39, 'M', "A la grande le puse cuca");
+        Homero.saySomething();
+        
+        Character Bart = new Character("Bart", 10, 'M', "Ay caramba!");
+        Bart.saySomething();
+    }
+        
+    
+    static class Character {
+        String name;
+        int age;
+        char gender;
+        String catchPhrase;
+        
+        private Character(String name, int age, char gender, String catchPhrase) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.catchPhrase = catchPhrase;
+    }
+        
+        void saySomething(){
+            System.out.println(this.catchPhrase);
+        }
     }
 }
